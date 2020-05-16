fd182817b73136066b88104cb1354a03445cddd8
takuya-nakazato103 <zaxy103@gmail.com>
2020-05-16 14:16:52 +0900

    configuration messages-rooting
    ルーティングの設定、グループにネストさせた
    該当アクションの定義

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:index, :new, :create, :edit, :update]
-  resources :messages, only: [:index, :new, :create, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:new, :create]
+  end
 end
