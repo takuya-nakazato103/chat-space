f8ab0ceb35f9701dd4a3331a7545a582ab5a525f
takuya-nakazato103 <zaxy103@gmail.com>
2020-05-30 15:09:22 +0900

    Update message.js
    自動更新の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -60,21 +60,32 @@ $(function() {
     .always(function() {
       $('.send__btn').removeAttr('disabled');
     });
-
-    var reloadMessages = function() {
-      var last_message_id = $('.message__container:last').data('message-id');
-      $.ajax({
-        url: "api/messages",
-        type: 'GET',
-        dataType: 'json',
-        data: {id: last_message_id}
-      })
-      .done(function() {
-        console.log('success');
-      })
-      .fail(function() {
-        console.log('error');
-      })
-    }
   });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.message__container:last').data('message-id');
+    $.ajax({
+      url: "api/messages",
+      type: 'GET',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = "";
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        $('.main__messages').append(insertHTML);
+        $('.main__messages').animate({ scrollTop: $('.main__messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messges/)) {
+    setInterval(reloadMessages, 7000);
+  }
+  
 });
