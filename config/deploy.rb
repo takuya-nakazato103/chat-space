7b2e0273f911238dcb94cc2f00452d665e04570e
takuya-nakazato103 <zaxy103@gmail.com>
2020-06-05 14:23:34 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ namespace :deploy do
   task :upload do
     on roles(:app) do |host|
       if test "[ ! -d #{shared_path}/config ]"
-        excute "mkdir -p #{shared_path}/config"
+        execute "mkdir -p #{shared_path}/config"
       end
       upload!('config/secrets.yml', "#{shared_path}/config/secrets.yml")
     end
